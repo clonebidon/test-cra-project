b518c711ef5b1d2f12cdfc52e736e5e9384ff4e9
sh1vadi <dimon29082007@gmail.com>
2024-04-17 18:14:30 +0300

    17.04_first

--- src/search/search.jsx
+++ src/search/search.jsx
@@ -1,11 +1,19 @@
+// import { Country, State, City } from 'country-state-city';
+//  // Получение городов штата Махараштра, Индия
+const data = require('countrycitystatejson');
 
+const countries = data.getCountries();
+const states = data.getStatesByShort('US');
+const cities = data.getCities('US', 'California');
+console.log(countries);
 const Search = ()=>{
-    let list_cite = fetch("https://github.com/umpirsky/country-list/tree/master/data")
-    console.log(list_cite)
     return(
-<select class="form-control" id="select">
-
-</select>
+        <>
+    <p>{}</p>
+    <select class="form-control" id="select">
+        <option velue='d'></option>
+    </select>
+</>
     )
 }
 export default Search;

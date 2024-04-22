c26c089c0dd676a466cf59ac5d77915b846a7a6c
lol002lol <142929902+MaxLakerCoding@users.noreply.github.com>
2024-04-22 19:58:43 +0300

    update2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './header.css';
 import logo from './planet.svg'
+import search from './search.svg'
 
 const Header = ()=>{
         return(
@@ -8,7 +9,8 @@ const Header = ()=>{
             <img className='logo' src={logo} alt='logo'/>
             <h1>KrytelykyWeather</h1>
             <input type="text"/>
-            <div className='searchButton'/>
+            <img src={search} className='searchButton'/>
+            <a href="https://github.com/clonebidon/test-cra-project" className="dev"/>
             <div className="donut"/>
         </div>
     )
